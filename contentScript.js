91c656f69119f9254a24b47832798f4478e6fa5e
mefa <mefaba@outlook.com>
2022-12-15 18:59:31 -0500

    fixing multiple triggers and lack of triggers

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,15 @@
-window.addEventListener("pageshow", () => {
-  let body = document.querySelector("#content");
-  nodeObserver(body, ModestHome);
+let observer = null;
+window.addEventListener("load", () => {
+  const elementToObserve = document.querySelector("#page-manager");
+  if(observer){ observer.disconnect();} //prettier-ignore
+  observer = nodeObserver(elementToObserve, ModestHome);
 });
 
 function ModestHome() {
   let home_videos = document.querySelector("#page-manager");
+
+  if (!home_videos) { return; } // prettier-ignore
+
   if (window.location.origin + "/" == window.location.href) {
     //home_videos.style.visibility = "hidden";
     home_videos.style.display = "none";
@@ -22,15 +27,18 @@ function nodeObserver(triggerNode, callFunction) {
   const targetNode = triggerNode;
 
   // Options for the observer (which mutations to observe)
-  const config = { attributes: true, childList: true, subtree: true };
-
+  const config = { attributes: true, childList: false, subtree: true };
   // Callback function to execute when mutations are observed
+  let timeout;
   const callback = function (mutationList, observer) {
-    //Trigger InsertBanner when new games appears on screen.
-    callFunction();
+    //debounce
+    clearTimeout(timeout);
+    timeout = setTimeout(() => callFunction(), 50);
   };
   // Create an observer instance linked to the callback function
   const observer = new MutationObserver(callback);
   // Start observing the target node for configured mutations
   observer.observe(targetNode, config);
+
+  return observer;
 }
